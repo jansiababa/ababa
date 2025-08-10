b11b4165bf7c6278b5301de4c198bcb3bef98e85
lecciones <leccionesjayric@gmail.com>
2025-08-10 13:40:58 -0700

    Added panel

--- src/app/main.java
+++ src/app/main.java
@@ -6,75 +6,93 @@ import java.util.Scanner;
 
 
 public class main {
-    public static void main(String[] args) {
-        System.out.println("Activity 1");
-        Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        
         
         System.out.println("HELLO WELCOME TO THE SYSTEM!");
-        System.out.println("What do you feel doing today? ");
-        System.out.println("1. Banking");
-        System.out.println("2. Doctors Appointment");
-        System.out.println("3. Shopping");
+        System.out.println("What is your choice for today!?");
+        System.out.println("1.Banking");
+        System.out.println("2.Doctors Appointment");
+        System.out.println("3.Shopping");
         
-        System.out.print("Enter choice: ");
-        int choice = sc.nextInt();
+        bankclass bApp[] = new bankclass[10];
+        int size = 0;
+        
+        System.out.print("Enter Choice: ");
+        int choice = sr.nextInt();
         
         switch(choice){
             case 1:
-       
-                 int attempts = 3;
-                        bankclass bc = new bankclass();
-
-                        System.out.print("Enter your Account No: ");
-                        int accountNo = sc.nextInt();
-
-                        System.out.print("Enter your Pin: ");
-                        int pin = sc.nextInt();
-
-                        while(!(bc.verifyAccount(accountNo, pin))){
-                            if(attempts == 1){
-                                System.out.println("ATTEMPT LIMIT REACHED!");
-                                System.exit(0);
-                            }
-
-                            attempts--;
-                            System.out.println("Attempt Left: "+attempts);
-
-                            System.out.println("INVALID ACCOUNT!");
-                            System.out.print("Enter your Account No: ");
-                            accountNo = sc.nextInt();
-                            System.out.print("Enter your Pin: ");
-                            pin = sc.nextInt();
-
-                        }
+                int resp;
+                do {
+                System.out.println("1.Register Account");
+                System.out.println("2.Login Account");
+                System.out.println("3.View All Accounts");
+                System.out.println("Enter Selection");
+                int action = sr.nextInt();
+                switch(action){
+                    case 1:
+                        bApp[size] = new bankclass();
                         
-
-                System.out.println("Enter your Account No: ");
-                 accountNo = sc.nextInt();
-
-                System.out.println("Enter your Pin: ");
-                 pin = sc.nextInt();
-              
-                    if(bc.verifyAccount(accountNo, pin)){
-                        System.out.println("LOGIN SUCCESS");
-                    }else{
-                        System.out.println("INVALID ACCOUNT!");
-                    }
-                
-                
+                        System.out.print("Enter Account No: ");
+                        bApp[size].setAccountNo(sr.nextInt());
+                        
+                        System.out.print("Enter Pin: ");
+                        bApp[size].setPin(sr.nextInt());
+                       
+                        System.out.println("Account Registered Successfully");
+                         size++;
+                        break;
+                    case 2:
+                       int attempts = 3;
+                       boolean found = false;
+                       
+                       while(attempts > 0 && !found){
+                           System.out.println("Enter Account No: ");
+                           int acc = sr.nextInt();
+                           
+                           System.out.println("Enter Pin: ");
+                           int pn = sr.nextInt();
+                           
+                           for(int i = 0; i < size; i++){
+                               if(bApp[i].verifyAccount(acc, pn)){
+                                   System.out.println("LOGIN SUCCESSFUL");
+                                   found = true;
+                                   break;
+                               }
+                                 }
+                               if(!found){
+                                 attempts--;
+                                 if(attempts < 0){
+                                     System.out.println("ATTEMPTS LIMIT REACHED!");
+                                     System.out.println("Access Denied.");
+                                 }else{
+                                     System.out.println("Login failed");
+                                     System.out.println("Attempts left: "+attempts);
+                                     System.exit(0);
+                               
+                               }
+                           }
+                       }
+                        break;
+                    default:
+                        
+                    case 3:
+                        break;
+                   
+                }
+                    System.out.println("Do you want to continue? (1 = Yes/No = 0)");
+                     resp = sr.nextInt();
+                }while(resp == 1);
                 break;
             case 2:
-                
                 break;
             case 3:
-                
                 break;
             default:
-                System.out.println("Invalid Selection!");
-        
+                System.out.println("Invalid! Section.");
+                System.out.println("Try again.");
         }
-            
     }
-
 }
-
--- src/bank/bankclass.java
+++ src/bank/bankclass.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bank;
 
-/**
- *
- * @author Angie
- */
 public class bankclass {
     
+      private int accountNo;
+    private float initialBalance;
+    private int pin;
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    public boolean verifyAccount(int acc, int pn){
+        
+            if(acc == accountNo && pn == pin){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    
+    public int setAccount(){
+        return accountNo;
+    }
+    
+    public void viewBalance(){
+    
+    
+    }
+    
+    
+    
 }
+
